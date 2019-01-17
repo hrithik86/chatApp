599633d2e945b5ae47f317f20857fa788d3543d3
hrithik86 <hrithikmalik86@gmail.com>
2019-01-17 19:59:17 +0530

    ui improvements & basic functionality

--- app.js
+++ app.js
@@ -1,20 +1,50 @@
-const express=require("express");
-const app=express();
-const path=require("path");
-const server =require("http").createServer(app);
-const io=require("socket.io").listen(server);
+const express = require('express');
+const path = require('path');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io').listen(server);
 
-//view engine
-app.set("views",path.join(__dirname,"views"));
-app.set("view engine","pug");
+let users = [];
 
-//static  path
-app.use(express.static(path.join(__dirname,"public")));
+const port = 3000;
 
-app.get("/",(req,res,next)=>{
-res.send("test");
+// View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// Static path
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Socket.io connect
+io.sockets.on('connection', (socket) => {
+  // Set Username
+  socket.on('set user', (data, callback) => {
+    console.log('setting');
+    if(users.indexOf(data) != -1){
+      callback(false);
+    } else {
+      callback(true);
+      socket.username = data;
+      users.push(socket.username);
+      updateUsers();
+    }
+  });
+
+  socket.on('disconnect', function(data){
+    if(!socket.username) return;
+    users.splice(users.indexOf(socket.username), 1);
+    updateUsers();
+  });
+
+  function updateUsers(){
+    io.sockets.emit('users', users);
+  }
 });
 
-server.listen(3000,()=>{
-console.log("server started on port 3000");
-})
+app.get('/', (req, res, next)=> {
+  res.render('index');
+});
+
+server.listen(port, () => {
+  console.log('Server started on port '+port);
+});
